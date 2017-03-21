5fef4961a67de48360a91400f37d7c9c11e061a9
akh-rman <akh-rman@rambler.ru>
2017-03-21 10:14:40 +0300

    Добавил новые материалы для объектов плоскость, куб и сфера. Внес исправления с рендером.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-// Как только все загружено, мы запускаем наш материал Three.js.
 $(function () {
     /**** Определили сцену, камеру и визуализатор *********/
     var scene = new THREE.Scene();
 	var camera = new THREE.PerspectiveCamera(45 
 		, window.innerWidth / window.innerHeight , 0.1, 1000);
 	var renderer = new THREE.WebGLRenderer();
-    renderer.setClearColor(new THREE.Color(0xEEEEEE));
+    renderer.setClearColor(0xEEEEEE, 1.0);
+    //renderer.setClearColor(new THREE.Color(0xEEEEEE));
 	renderer.setSize(window.innerWidth, window.innerHeight);
     /***********************************************/
     /******* Добавляем вспомогательные оси и плоскость *******/
 	var axes = new THREE.AxisHelper(20);
 	scene.add(axes);
 	var planeGeometry = new THREE.PlaneGeometry(70, 30);
-	var planeMaterial = new THREE.MeshBasicMaterial({color: 0xcccccc});
+	var planeMaterial = new THREE.MeshLambertMaterial({color: 0xcccccc});
 	var plane = new THREE.Mesh(planeGeometry,planeMaterial);
 	plane.rotation.x = -0.5*Math.PI;
 	plane.position.x = 15;
@@ -22,7 +22,7 @@ $(function () {
     /******************************/
     /******* Добавлем куб *******/
 	var cubeGeometry = new THREE.CubeGeometry(4, 4, 4);
-	var cubeMaterial = new THREE.MeshBasicMaterial(
+	var cubeMaterial = new THREE.MeshLambertMaterial(
 		{color: 0xff0000, wireframe: false});
 	var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 	cube.position.x = -4;
@@ -30,15 +30,20 @@ $(function () {
 	cube.position.z = 0;
 	scene.add(cube);
     /******************************/
-    /***** Добавляем сферу *********/
+    /******* Добавляем сферу *********/
 	var sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
-	var sphereMaterial = new THREE.MeshBasicMaterial(
+	var sphereMaterial = new THREE.MeshLambertMaterial(
 		{color: 0x7777ff, wireframe: false});
 	var sphere = new THREE.Mesh(sphereGeometry,sphereMaterial);
 	sphere.position.x = 20;
 	sphere.position.y = 4;
 	sphere.position.z = 2;
 	scene.add(sphere);
+    /******************************/
+    /****** Добавляем освещение *******/
+    var spotLight = new THREE.SpotLight(0xffffff);
+    spotLight.position.set(-40, 60, -10);
+    scene.add(spotLight);
     /******************************/
     /****** Позиционируем камеру и запускаем рендер ******/
 	camera.position.x = -30;
@@ -47,10 +52,5 @@ $(function () {
 	camera.lookAt(scene.position);
 	$("#WebGL-output").append(renderer.domElement);
 	renderer.render(scene, camera);
-    /******************************/
-    /****** Добавляем материалы и освещение *******/
-    var spotLight = new THREE.SpotLight(0xffffff);
-    spotLight.position.set(-40, 60, -10);
-    scene.add(spotLight);
     /******************************/
 });
